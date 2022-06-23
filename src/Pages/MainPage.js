ffb929ef5d9ee9f297387258a3f58ac4bd16b346
Desire <desireirankundawilliams@gmail.com>
2022-06-23 13:46:57 +0200

    modified the mainpage component

--- src/Pages/MainPage.js
+++ src/Pages/MainPage.js
@@ -9,12 +9,7 @@ const MainPage = () => {
         <Container>
             <br />
             <Row>
-                <Col>
-                    <Card.Body>
-                        <Card.Img variant="top" src={desire} />
-                    </Card.Body>
-                </Col>
-                <Col>
+            <Col>
                     <Card.Text>
                         <br />
                         <br />
@@ -25,13 +20,19 @@ const MainPage = () => {
                     <Card.Text>
                         <br />
                         <br />
-                        <p>I enjoy working on challenges that help me fulfill the goals of the 
+                        <p style={{textAlign: 'left', fontSize: '200'}} >I enjoy working on challenges that help me fulfill the goals of the 
                             organisation. I am very passionate about learning new technologies 
                             and working with a bigger team to learn new things and share my knowledge 
                             with others. I have good communication skills that enable me to develop a good 
                             rapport with people around me. I am constantly looking for opportunities to  help me grow my technical skills.</p>
                     </Card.Text>
                 </Col>
+                <Col>
+                    <Card.Body>
+                        <Card.Img variant="top" style={{}} height={500} width={400} src={desire} />
+                    </Card.Body>
+                </Col>
+                
             </Row>
         </Container>
     )
