17407e60bc63de50d5062b112e6d6a44a8fd4182
Desire <desireirankundawilliams@gmail.com>
2022-10-26 23:30:34 +0200

    able to display am aim on the ui

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,36 +1,57 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
-import Grid from '@material-ui/core/Grid';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import Button from "@material-ui/core/Button";
+import Grid from "@material-ui/core/Grid";
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
-    marginTop: 100
-
+    marginTop: 100,
+  },
+  h3: {
+    fontSize: 50,
   },
   paper: {
     padding: theme.spacing(2),
-    textAlign: 'center',
+    textAlign: "center",
     color: theme.palette.text.secondary,
   },
 }));
 
-
 const Home = () => {
-    const classes = useStyles();
+  const classes = useStyles();
   return (
     <div className={classes.root}>
       <Grid container spacing={3}>
         <Grid item xs={6}>
-          <Paper className={classes.paper}>xs=6</Paper>
+          <h3 className={classes.h3}>
+            Hi, I am Desire,
+            <br />
+            Software Developer
+          </h3>
+          <p style={{ textAlign: "justify" }}>
+            I enjoy working on challenges that help me fulfill the goals of the
+            organisation. I am very passionate about learning new technologies
+            and working with a bigger team to learn new things and share my
+            knowledge with others. I have good communication skills that enable
+            me to develop a good rapport with people around me. I am constantly
+            looking for opportunities to help me grow my technical skills.
+          </p>
+          <Button
+            variant="contained"
+            color="primary"
+            style={{ backgroundColor: "green" }}
+          >
+            Download Resume
+          </Button>
         </Grid>
         <Grid item xs={6}>
           <Paper className={classes.paper}>xs=6</Paper>
         </Grid>
       </Grid>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
